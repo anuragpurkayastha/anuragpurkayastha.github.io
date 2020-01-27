7c8fac29db7477dd50e22dd7d2b7b74cad27330d
anuragpurkayastha <anuragpurkayastha@gmail.com>
2020-01-27 23:52:50 +1100

    Formatting and colour changes.

--- js/GuessTheColourGameScript.js
+++ js/GuessTheColourGameScript.js
@@ -50,15 +50,40 @@ function startGame(){
 
 	// Initialize the display of the page.
 	answerMessage.innerHTML = "";	//	Clear the answer message
+	document.getElementsByTagName("body")[0].style.backgroundColor = "#45678a";
 	
-	var answerButtonIndex = Math.round(Math.random() * (buttons.length - 1));	//Index of the randomly selected "correct" answer button.
+	for (let i = 0; i < buttons.length ; i++){
+		//Listen for clicks on each button
+		buttons[i].removeEventListener('click', function() {
+			
+			console.log("Button " + i + " has been clicked");
+			
+			if (buttons[i] === buttons[answerButtonIndex]){
+				// Change the answer message appropriately
+				answerMessage.innerHTML = "Correct answer!";
+				
+				// Change the colour of the body to colour of the answer button.
+				let answerButtonColourVal = window.getComputedStyle(buttons[answerButtonIndex],null).getPropertyValue("background-color");
+				document.getElementsByTagName("body")[0].style.backgroundColor = answerButtonColourVal;
+			}else{
+				answerMessage.innerHTML = "Wrong answer! Try again.";
+			}
+		});
+	}
+	
+	let answerButtonIndex = -1;
 
+	do {
+		answerButtonIndex = Math.round(Math.random() * buttons.length) - 1;
+	}while (answerButtonIndex < 0);
+	
 	console.log("Answer button index:" + answerButtonIndex);
+	
 	//Randomly change all of the button colours
 	for (let i = 0; i < buttons.length ; i++) {
 		let randomRGBColour = makeRandomColour();	//Get a random RGB colour
 		setButtonColour(buttons[i] , randomRGBColour);	//Set the button colour to the random colour.
-
+		
 		// If i is equal to the index of the randomly selected "answer button", display the
 		// rgb colour value of the answer button.
 		if (i === answerButtonIndex){
@@ -67,13 +92,27 @@ function startGame(){
 
 		//Listen for clicks on each button
 		buttons[i].addEventListener('click', function() {
-			if (this === buttons[answerButtonIndex]){
+			
+			console.log("Button " + i + " has been clicked");
+			
+			if (buttons[i] === buttons[answerButtonIndex]){
+				// Change the answer message appropriately
 				answerMessage.innerHTML = "Correct answer!";
-				document.getElementsByTagName("body")[0].style.backgroundColor = "rgb(" + randomRGBColour[0] + "," + randomRGBColour[1] + "," + randomRGBColour[2] + ")";
+				
+				// Change the colour of the body to colour of the answer button.
+				let answerButtonColourVal = window.getComputedStyle(buttons[answerButtonIndex],null).getPropertyValue("background-color");
+				document.getElementsByTagName("body")[0].style.backgroundColor = answerButtonColourVal;
 			}else{
 				answerMessage.innerHTML = "Wrong answer! Try again.";
 			}
 		});
+	}
+	
+	/*---------------------------------------------------------------------------------------------
+								DEBUGGING PURPOSES
+	----------------------------------------------------------------------------------------------*/
+	for (let i = 0; i < buttons.length ; i++){
+		console.log("The colour of button[" + i + "] is: " + buttons[i].style.backgroundColor);
 	}
 }
 
