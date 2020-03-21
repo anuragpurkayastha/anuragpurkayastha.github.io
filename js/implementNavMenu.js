66b3235a203fa3b0b4349000ed684559c6907ff3
anuragpurkayastha <anuragpurkayastha@gmail.com>
2020-03-21 18:40:17 +1100

    Moved website to local Xampp server

---
+++ js/implementNavMenu.js
@@ -0,0 +1,4 @@
+//This is a script to include the navigation html code in each page.
+$(function(){
+	$( "#navigation" ).load("navMenu.html");
+});
---
+++ js/modifyNavMenu.js
@@ -0,0 +1,3 @@
+$(function(){
+	$( "#navigationMenu" ).append("<li class = 'nav-item'><a class='nav-link'>Home</a></li>");
+});
