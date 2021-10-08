7e52bc5a16b13df589dc6e75ecd4b942aeb4effa
Anurag <anuragpurkayastha@gmail.com>
2021-10-08 23:06:20 +1100

    First blog commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to my tech blog!",
+        "excerpt":"This is my blog which will be about a whole heap of tech stuff! From linux to programming to web development to networking! I will be adding posts to this blog as time allows because I am also working ;). I’ll be covering topics that I find particularly interesting. For...","categories": [],
+        "tags": [],
+        "url": "/welcome-to-my-blog/",
+        "teaser": null
+      }]
