4d184881ec9cee0a0256e4d15480ccaada4c5538
Anurag <anuragpurkayastha@gmail.com>
2021-10-16 17:41:08 +1100

    Added Designing an App Part 1 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,5 +3,11 @@ var store = [{
         "excerpt":"This is my blog which will be about a whole heap of tech stuff! From linux to programming to web development to networking! I will be adding posts to this blog as time allows because I am also working ;). I’ll be covering topics that I find particularly interesting. For...","categories": [],
         "tags": [],
         "url": "/welcome-to-my-blog/",
-        "teaser": null
+        "teaser": "/assets/computer.jpg"
+      },{
+        "title": "Designing An App - Part 1",
+        "excerpt":"Recently I had to complete a week long coding test for a job application I submitted. Fortunately, the company found my submission of acceptable quality and I made it through to the next round. This post is about the design process I undertook to program the application. The Game The...","categories": [],
+        "tags": [],
+        "url": "/Designing-An-App-Part-1/",
+        "teaser": "/assets/monopoly.jpg"
       }]
